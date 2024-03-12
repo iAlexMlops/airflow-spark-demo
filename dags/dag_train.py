67f7671a35ce611c0dfb2e8ea6822a6ebd5cafcd
Alex Egorov <a.egorov@ktsstudio.ru>
2024-03-13 01:50:00 +0300

    Add dags and spark applications

--- dags/dag_train.py
+++ dags/dag_train.py
@@ -2,21 +2,14 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 
 
-DESCRIPTION = "Description for train dag"
-DOC_MD = """
-Train dag for demo
-"""
-
 with DAG(
     dag_id="model_train",
-    description=DESCRIPTION,
     catchup=False,
-    doc_md=DOC_MD,
 ) as dag:
 
     train_model = BashOperator(
         task_id='train_model',
-        bash_command='python scripts/train.py',
+        bash_command='tree && python scripts/train.py',
         dag=dag,
     )
 
