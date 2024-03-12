a12aff8c6340d8d230426215608e917f2e27f3c5
Alex Egorov <a.egorov@ktsstudio.ru>
2024-03-13 00:59:16 +0300

    Init

---
+++ dags/dag_train.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+
+
+DESCRIPTION = "Description for train dag"
+DOC_MD = """
+Train dag for demo
+"""
+
+with DAG(
+    dag_id="{{ dag_name }}",
+    description=DESCRIPTION,
+    catchup=False,
+    doc_md=DOC_MD,
+) as dag:
+    task_submit = SparkKubernetesOperator(
+        task_id='submit',
+        namespace="airflow",
+        application_file="local:///app/train.py",
+        do_xcom_push=True,
+        dag=dag
+    )
+
+    task_wait = SparkKubernetesSensor(
+        task_id='wait',
+        namespace="airflow",
+        application_name="{{ task_instance.xcom_pull(task_ids='submit')['metadata']['name'] }}",
+        dag=dag,
+        attach_log=True,
+    )
+
+    task_submit >> task_wait
