29360e3972a702c75aa10e5528948be514138ef6
Alex Egorov <a.egorov@ktsstudio.ru>
2024-03-13 13:07:12 +0300

    Downgrade AirFlow

--- dags/dag_train.py
+++ dags/dag_train.py
@@ -1,16 +1,33 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 
+
+DESCRIPTION = "Description for train dag"
+DOC_MD = """
+train dag for demo
+"""
 
 with DAG(
     dag_id="model_train",
+    description=DESCRIPTION,
     catchup=False,
+    doc_md=DOC_MD,
 ) as dag:
-
-    train_model = BashOperator(
-        task_id='train_model',
-        bash_command='find . && python scripts/train.py',
-        dag=dag,
+    task_submit = SparkKubernetesOperator(
+        task_id='submit',
+        namespace="airflow",
+        application_file="sparkapplications/train.yaml",
+        do_xcom_push=True,
+        dag=dag
     )
 
-    train_model
+    task_wait = SparkKubernetesSensor(
+        task_id='wait',
+        namespace="airflow",
+        application_name="{{ task_instance.xcom_pull(task_ids='submit')['metadata']['name'] }}",
+        dag=dag,
+        attach_log=True,
+    )
+
+    task_submit >> task_wait
