d6c2d0010b2394a08976e0bb5052bda66fb02a2e
Alex Egorov <a.egorov@ktsstudio.ru>
2024-03-13 19:40:42 +0300

    Test dags

--- dags/dag_test.py
+++ dags/dag_test.py
@@ -1,57 +1,42 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-"""
-This is an example dag for using the KubernetesJobOperator.
-"""
-from __future__ import annotations
-
 import os
-from datetime import datetime
+import pathlib
+from datetime import datetime, timedelta
+from os.path import join
 
+# [START import_module]
+# The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.job import KubernetesJobOperator
+
+# Operators; we need this to operate!
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+
 
 ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
-DAG_ID = "example_kubernetes_job_operator"
+DAG_ID = "spark_pi"
 
 with DAG(
-    dag_id=DAG_ID,
-    schedule=None,
+    DAG_ID,
+    default_args={"max_active_runs": 1},
+    description="submit spark-pi as sparkApplication on kubernetes",
+    schedule=timedelta(days=1),
     start_date=datetime(2021, 1, 1),
-    tags=["example", "kubernetes"],
+    catchup=False,
 ) as dag:
-    # [START howto_operator_k8s_job]
-    k8s_job = KubernetesJobOperator(
-        task_id="job-task",
+    # [START SparkKubernetesOperator_DAG]
+    pi_example_path = pathlib.Path(__file__).parent.resolve()
+    t1 = SparkKubernetesOperator(
+        task_id="spark_pi_submit",
         namespace="default",
-        image="perl:5.34.0",
-        cmds=["perl", "-Mbignum=bpi", "-wle", "print bpi(2000)"],
-        name="test-pi",
+        application_file=join(pi_example_path, "example_spark_kubernetes_spark_pi.yaml"),
+        do_xcom_push=True,
+        dag=dag,
     )
-    # [END howto_operator_k8s_job]
 
-    from tests.system.utils.watcher import watcher
-
-    # This test needs watcher in order to properly mark success/failure
-    # when "tearDown" task with trigger rule is part of the DAG
-    list(dag.tasks) >> watcher()
-
-from tests.system.utils import get_test_run  # noqa: E402
-
-# Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
-test_run = get_test_run(dag)
+    t2 = SparkKubernetesSensor(
+        task_id="spark_pi_monitor",
+        namespace="default",
+        application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
+        dag=dag,
+    )
+    t1 >> t2
